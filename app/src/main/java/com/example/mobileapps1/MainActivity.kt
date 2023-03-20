95a1d6d3df2f03d5a51ebeead7235730a5e48090
priirocha <prii.rocha@hotmail.com>
2023-03-20 14:58:54 +0000

    Creating new activity pages

--- app/src/main/java/com/example/mobileapps1/MainActivity.kt
+++ app/src/main/java/com/example/mobileapps1/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.mobileapps1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.core.content.ContextCompat.startActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val clickEducation: Button = findViewById(R.id.button4)
+        clickEducation.setOnClickListener { clickEducation() }
     }
+}
+
+private fun clickEducation() {
+
 }
